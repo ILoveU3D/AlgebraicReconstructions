332b157f3674f5dd8df27e158a45dce12fc26aa8
王煜康 <ywan0633@student.monash.edu>
2022-09-06 15:04:04 +0800

    feat: none fix: none docs: none style: none refactor: none perf: put operations in l1-ball into GPU test: none chore: none

--- Algorithm/DTV/l1ball.py
+++ Algorithm/DTV/l1ball.py
@@ -2,17 +2,16 @@ import torch
 import numpy as np
 
 def l1ball(v,b):
-    v = v.cpu().numpy()
-    if (np.sum(np.abs(v)) < b):
-        return torch.from_numpy(v).cuda()
+    if (torch.sum(torch.abs(v)) < b):
+        return v
     paramLambda = 0
-    objectValue = np.sum(np.max(np.abs(v)-paramLambda,0)) - b
+    objectValue = torch.sum(torch.nn.functional.relu(torch.abs(v)-paramLambda)) - b
     iterations = 0
-    while(np.abs(objectValue) > 1e-4 and iterations < 100):
-        objectValue = np.sum(np.max(np.abs(v) - paramLambda, 0)) - b
-        difference = np.sum(-np.where(np.abs(v)-paramLambda > 0,1,0)) + 0.001
-        paramLambda -= objectValue / difference
+    while(torch.abs(objectValue) > 1e-4 and iterations < 100):
+        objectValue = torch.sum(torch.nn.functional.relu(torch.abs(v) - paramLambda)) - b
+        difference = torch.sum(-torch.where(torch.abs(v)-paramLambda > 0,1,0)) + 0.001
+        paramLambda -= (objectValue / difference).item()
         iterations += 1
     paramLambda = np.max(paramLambda, 0)
-    w = np.sign(v) * np.max(np.abs(v)-paramLambda,0)
-    return torch.from_numpy(w).cuda()
+    w = torch.sign(v) * torch.nn.functional.relu(torch.abs(v)-paramLambda)
+    return w
